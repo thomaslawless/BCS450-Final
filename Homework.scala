8b1feff8da0bddea64f188e9da413b75f582c499
harrtm <75387730+harrtm@users.noreply.github.com>
2020-12-09 13:59:52 -0500

    Updated Homework.scala
    Update Homework.scala to include type annotations for the getters so that intelliJ doesn't complain.

--- Homework.scala
+++ Homework.scala
@@ -9,37 +9,37 @@ class Homework(var inputname: String, var inputDueDate: String)
   private var _completed = false
 
   //Name Getter and Setter
-  def name = _name
+  def name: String = _name
   def name_= (newValue:String):Unit = {
     _name = newValue
   }
 
   //due date getter and setter
-  def dueDate = _dueDate
+  def dueDate: String = _dueDate
   def dueDate_= (newValue:String):Unit = {
     _dueDate = newValue
   }
 
   //Time completed getter and setter
-  def timeCompleted = _timeCompleted
+  def timeCompleted: Int = _timeCompleted
   def timeCompleted_= (newValue:Int):Unit={
     _timeCompleted = newValue
   }
 
   //date completed getter and setter
-  def dateCompleted = _dateCompleted
+  def dateCompleted: String = _dateCompleted
   def dateCompleted_= (newValue:String):Unit={
     _dateCompleted = newValue
   }
 
   //Grade getter and setter
-  def grade = _grade
+  def grade: Int = _grade
   def grade_= (newValue:Int):Unit={
     _grade = newValue
   }
 
   //completed getter and setter
-  def completed = _completed
+  def completed: Boolean = _completed
   def completed_= (newValue: Boolean):Unit={
     _completed = newValue
   }

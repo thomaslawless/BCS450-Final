5fc25a1d32214a86ae3dc56ae36b9f2c9b28f1ec
abdulnad <53980282+abdulnad@users.noreply.github.com>
2020-12-12 16:14:14 -0500

    Added my HomeworkSchdule File

---
+++ HomeworkSchedule.scala
@@ -0,0 +1,37 @@
+package com.abdulsproject
+
+import scala.collection.mutable.ListBuffer
+
+
+class HomeworkSchedule {
+  //private var hws : List[Homework] = List()
+  private var hws = new ListBuffer[Homework]()
+  private var size = 0
+
+  //Add Homework to Schedule with name and due date
+  def addHW(name: String)(dueDate: String): Unit = {
+    val hw = new Homework(name, dueDate);
+    hws += hw;
+    size = size + 1
+  }
+
+  //Add Homeowkr to Schedule with predefined homework object
+  def addHW(h: Homework): Unit = hws += h
+
+  //return number of homeworks in schedule
+  def getSize: Int = size
+
+  //Retrieve homework from homework schedule
+  def getHW(HWname: String)(HWdueDate: String): Boolean = {
+    val hwsList = hws.toList
+    var a = 0
+    for (a <- 0 to size - 1) {
+      var theName = hwsList(a).name
+      var theDueDate = hwsList(a).dueDate
+      println("theName : " + theName + " theDueDate: " + theDueDate)
+      if (HWname == theName && HWdueDate == theDueDate)
+        return true
+    }
+    return false
+  }
+}

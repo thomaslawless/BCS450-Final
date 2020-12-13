b97ccd2d3614089871698acc6c7a18d1f18d9589
harrtm <75387730+harrtm@users.noreply.github.com>
2020-12-13 13:50:31 -0500

    Updated main.scala to include more comments

--- main.scala
+++ main.scala
@@ -4,22 +4,29 @@ object main {
     val a = new Homework("Homework 1", "12/2")
     markHomeworkComplete(a, 1200)
   }
+  //Polymorphic function, is used to mark homeworks as complete through a variety of means. Accepts boolean, string, double, or integer. Boolean is for a simple
+  //marking of homework as completed. String is for setting the date completed and the homework as completed. Double is for setting the grade and the homework as
+  //completed. Integer is for setting the time the homework was completed. Grade is a double to enable differentiation between grade and time, has the added
+  // bonus of allowing grades to be decimals.
   def markHomeworkComplete[A](homework: Homework, input: A): Unit = {
     println(input.getClass.getSimpleName)
     if(input.getClass.getSimpleName == "Boolean") {
       homework.completed = true
     }
-    if(input.getClass.getSimpleName == "String"){
+    else if(input.getClass.getSimpleName == "String"){
       homework.dateCompleted = input.toString
       homework.completed = true
     }
-    if(input.getClass.getSimpleName == "Double"){
+    else if(input.getClass.getSimpleName == "Double"){
       homework.grade = extractDouble(input)
       homework.completed = true
     }
-    if(input.getClass.getSimpleName == "Integer"){
+    else if(input.getClass.getSimpleName == "Integer"){
       homework.timeCompleted = extractInt(input)
       homework.completed = true
+    }
+    else{
+      println("Invalid input on function markHomeworkComplete")
     }
     println(homework.name + " is now marked as completed.")
   }
@@ -27,6 +34,7 @@ object main {
   def extractDouble(x: Any): Double = x match {
     case n: java.lang.Number => n.doubleValue()
   }
+  //Converts an object of type A into Int, used in markHomeworkComplete.
   def extractInt(x: Any): Int = x match {
     case n: java.lang.Number => n.intValue()
   }
